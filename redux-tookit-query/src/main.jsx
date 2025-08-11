68baa556de682d3faf22db3dd45b410489b31bd1
Sunil-Patel-21 <patelsunil60273@gmail.com>
2025-08-11 10:44:40 +0530

    CRUD with RTK query

--- redux-tookit-query/src/main.jsx
+++ redux-tookit-query/src/main.jsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import Home from "./Home.jsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import "./index.css";
+import { ApiProvider } from "@reduxjs/toolkit/query/react";
+import { api } from "./apiSlice.js";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <ApiProvider api={api}>
+        <App />
+      </ApiProvider>
+    ),
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/contact",
+        element: <p>Contact Us</p>,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
